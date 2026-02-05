2725e21601f22b8050ac03f0fc5d1c378b9989c6
JuanDiazA26 <202310687@alu.comillas.edu>
2026-02-05 22:01:44 +0100

    Conversión Bronce Plata

---
+++ bronce2silver.py
@@ -0,0 +1,82 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, trim, lower, to_date, when, to_timestamp, regexp_replace, from_unixtime
+import boto3
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+from botocore.exceptions import ClientError
+
+def delete_bucket_if_exists(bucket_name: str):
+    try:
+        # Comprobamos si el bucket existe
+        s3.head_bucket(Bucket=bucket_name)
+        print(f"El bucket {bucket_name} existe. Borrándolo...")
+
+        # Listar y borrar objetos
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        if "Contents" in response:
+            objects = [{"Key": obj["Key"]} for obj in response["Contents"]]
+            s3.delete_objects(
+                Bucket=bucket_name,
+                Delete={"Objects": objects}
+            )
+
+        # Borrar el bucket
+        s3.delete_bucket(Bucket=bucket_name)
+        print(f"Bucket eliminado: {bucket_name}")
+
+    except ClientError as e:
+        error_code = e.response["Error"]["Code"]
+        if error_code == "404":
+            print(f"El bucket {bucket_name} no existe. Continuamos...")
+        else:
+            print(f"Error borrando bucket {bucket_name}: {e}")
+            raise
+
+
+def create_bucket(bucket_name: str, region: str = "eu-west-1"):
+    try:
+        if region == "us-east-1":
+            s3.create_bucket(Bucket=bucket_name)
+        else:
+            s3.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={"LocationConstraint": region},
+            )
+
+        print(f"Bucket creado: {bucket_name} ({region})")
+        return True
+
+    except ClientError as e:
+        print(f"Error creando bucket: {e}")
+        return False
+
+if __name__ == "__main__":
+    s3 = boto3.client("s3", region_name="eu-south-2")
+    bucket_bronce = "bronceimat3a03"
+    bucket_plata = "plataimat3a03"
+    delete_bucket_if_exists(bucket_plata)
+    create_bucket(bucket_name=bucket_plata, region="eu-south-2")
+    spark = SparkSession.builder \
+        .appName("BronceToPlata_Transformation") \
+        .getOrCreate()
+
+    path_bronce = "s3://bronceimat3a03/"
+    path_plata = "s3://plataimat3a03/data"
+    
+    schema = StructType([
+    StructField("datetime", TimestampType(), True),
+    StructField("symbol", StringType(), True),
+    StructField("open", DoubleType(), True),
+    StructField("high", DoubleType(), True),
+    StructField("low", DoubleType(), True),
+    StructField("close", DoubleType(), True),
+    StructField("volume", DoubleType(), True)])
+
+    df_raw = spark.read.format("csv") \
+        .option("header", "True") \
+        .option("sep", ",") \
+        .schema(schema) \
+        .load(path_bronce)
+
+    df_raw.write.mode("overwrite") \
+        .partitionBy("crypto", "year", "month") \
+        .parquet(path_plata)
